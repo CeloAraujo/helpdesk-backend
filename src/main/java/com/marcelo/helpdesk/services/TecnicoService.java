b632bd185170f5eaf86b6ef7278139dc12077f74
CeloAraujo <celoparaujo99@gmail.com>
2024-07-25 16:19:38 -0300

    endpoint create para Tecnico

--- src/main/java/com/marcelo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcelo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.helpdesk.domain.Tecnico;
+import com.marcelo.helpdesk.domain.dtos.TecnicoDTO;
 import com.marcelo.helpdesk.repositories.TecnicoRepository;
 import com.marcelo.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -24,5 +25,14 @@ public class TecnicoService {
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+	}
+
+	
 
 }
