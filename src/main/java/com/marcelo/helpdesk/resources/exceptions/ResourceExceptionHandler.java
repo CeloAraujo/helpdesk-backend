c79bae4b186a141430adab5a83fe8c74317c21ca
CeloAraujo <celoparaujo99@gmail.com>
2024-07-25 16:56:47 -0300

    exceção para cpf e email já cadastrados

--- src/main/java/com/marcelo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/marcelo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.marcelo.helpdesk.services.exceptions.DataIntegrityViolation;
 import com.marcelo.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @ControllerAdvice
@@ -15,10 +16,20 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectnotFoundException.class)
 	public ResponseEntity<StandardError> objectnotFoundException(ObjectnotFoundException ex,
 			HttpServletRequest request) {
-		
+
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				"Object not found", ex.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	@ExceptionHandler(DataIntegrityViolation.class)
+	public ResponseEntity<StandardError> dataIntegrityViolation(DataIntegrityViolation ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/marcelo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcelo/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcelo.helpdesk.domain.Pessoa;
 import com.marcelo.helpdesk.domain.Tecnico;
 import com.marcelo.helpdesk.domain.dtos.TecnicoDTO;
+import com.marcelo.helpdesk.repositories.PessoaRepository;
 import com.marcelo.helpdesk.repositories.TecnicoRepository;
+import com.marcelo.helpdesk.services.exceptions.DataIntegrityViolation;
 import com.marcelo.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
@@ -16,6 +19,8 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -29,8 +34,17 @@ public class TecnicoService {
 	public Tecnico create(TecnicoDTO objDTO) {
 		
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolation("CPF já cadastrado no sistema!");
+		}
+		
 	}
 
 	
